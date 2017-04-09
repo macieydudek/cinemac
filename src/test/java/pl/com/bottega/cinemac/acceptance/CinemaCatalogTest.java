0dcd3201f8ef16d182de30e6278d9eb191b9c3dd
macieydudek <macieydudek@gmail.com>
2017-04-09 15:50:19 +0200

    story: >>>As a customer I want to see a list of cinemas<<< started

--- src/test/java/pl/com/bottega/cinemac/acceptance/CinemaCatalogTest.java
+++ src/test/java/pl/com/bottega/cinemac/acceptance/CinemaCatalogTest.java
@@ -5,9 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest
-@Transactional
 public class CinemaCatalogTest {
-    
+
 }
