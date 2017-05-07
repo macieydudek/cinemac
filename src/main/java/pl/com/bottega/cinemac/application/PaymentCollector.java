31d87bdac44737e775cf51adc64cdc54ee203414
macieydudek <macieydudek@gmail.com>
2017-05-07 09:34:31 +0200

    quick bugfix after refatoring

--- src/main/java/pl/com/bottega/cinemac/application/PaymentCollector.java
+++ src/main/java/pl/com/bottega/cinemac/application/PaymentCollector.java
@@ -1,9 +1,10 @@
 package pl.com.bottega.cinemac.application;
 
 import pl.com.bottega.cinemac.model.commands.CollectPaymentCommand;
+import pl.com.bottega.cinemac.model.reservation.PaymentAttempt;
 
 public interface PaymentCollector {
 
-    void collectPayment(CollectPaymentCommand cmd);
+    PaymentAttempt collectPayment(CollectPaymentCommand cmd);
 
 }
